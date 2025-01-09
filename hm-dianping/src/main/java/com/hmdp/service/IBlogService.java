7c77b89c654d0fecc61165a216f702dc75074f4c
dive-back-in-time <1531438238@qq.com>
2025-01-09 16:21:02 +0800

    完成了点赞和点赞排行榜的功能

--- hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,26 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    /**
+     * 根据id查询博客
+     * @param id
+     * @return
+     */
+    Result queryBlogById(Long id);
+
+    /**
+     * 实现点赞功能
+     * @param id
+     * @return
+     */
+    Result likeBlog(Long id);
+
+    Result queryHotBlog(Integer current);
+
+    /**
+     * 查询点赞排行前五的用户
+     * @param id
+     * @return
+     */
+    Result queryBlogLikes(Long id);
 }
